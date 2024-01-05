73cc562d9e313adef9688f7e1d56bd69908d1625
Ahmed Elnemr <aelnemr711@gmail.com>
2024-01-06 01:30:47 +0200

    Finish Notes Bottom Sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
+import 'custom_button.dart';
+
 class AddBottomSheet extends StatelessWidget {
   const AddBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/Constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+          color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+      child: Center(
+          child: Text(
+        'Add',
+        style: TextStyle(
+            color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
