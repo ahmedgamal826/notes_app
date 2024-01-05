cb96cc331c117c72b2ee62aaae7c9262a0f3667a
Ahmed Elnemr <aelnemr711@gmail.com>
2024-01-05 20:30:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
