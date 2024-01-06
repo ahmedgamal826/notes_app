79008017daf49aa034088c24fba29be9b6edd0af
Ahmed Elnemr <aelnemr711@gmail.com>
2024-01-06 22:05:48 +0200

    Create Edit Note View

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 18, top: 24, bottom: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                'Flutter Tips',
-                style: TextStyle(fontSize: 26, color: Colors.black),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 16),
-                child: Text(
-                  'Build your career with ahmed elnemr',
-                  style: TextStyle(
-                      fontSize: 18, color: Colors.black.withOpacity(.5)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18, top: 24, bottom: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  'Flutter Tips',
+                  style: TextStyle(fontSize: 26, color: Colors.black),
                 ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 16),
+                  child: Text(
+                    'Build your career with ahmed elnemr',
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.black.withOpacity(.5)),
+                  ),
+                ),
+                trailing: IconButton(
+                    onPressed: () {
+                      print('delete successfully');
+                    },
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                      size: 30,
+                    )),
               ),
-              trailing: IconButton(
-                  onPressed: () {
-                    print('delete successfully');
-                  },
-                  icon: Icon(
-                    Icons.delete,
-                    color: Colors.black,
-                    size: 30,
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Text(
-                'Jaunary 5, 2024',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.4), fontSize: 16),
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Text(
+                  'Jaunary 5, 2024',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.4), fontSize: 16),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
