62d6bf90313e446d3586bb80cef46be7e3511e6f
Ahmed Elnemr <aelnemr711@gmail.com>
2024-01-06 23:38:57 +0200

    ibit hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/note_view.dart';
+import 'package:notes_app/views/widgets/constants.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+
   runApp(const NotesApp());
 }
 
--- lib/views/widgets/constants.dart
+++ lib/views/widgets/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = const Color(0xff62FCD7);
+const kNotesBox = 'notes_box';
